f85aea26cd3ba8623e9377284f99febd397e2203
tyedem <90783116+tyedem@users.noreply.github.com>
2022-03-25 10:35:39 -0400

    Correct code

--- pychain.py
+++ pychain.py
@@ -186,7 +186,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=Record,
+        record=List[Record],
         creator_id=42,
         prev_hash=prev_block_hash
     )
